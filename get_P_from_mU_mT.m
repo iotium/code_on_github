6503916d2b16e3b7c26c174775c631091ea0a187
iotium <jonah.e.zimmerman@gmail.com>
2014-11-10 22:54:00 -0800

    improved N2O fits for saturation properties
    now it runs faster than using refprop

--- get_P_from_mU_mT.m
+++ get_P_from_mU_mT.m
@@ -11,13 +11,13 @@ end
 
     function F = eqns_to_solve(P)
         
-         rho_tg = refpropm('D', 'P', P/1e3, 'U', U_tg/m_tg, 'N2O');
+%          rho_tg = refpropm('D', 'P', P/1e3, 'U', U_tg/m_tg, 'N2O');
          
-%         [rho_tg_l, rho_tg_v, u_tg_l, u_tg_v] = n2o_fits_for_getting_P(P);
-%         u_tg = U_tg/m_tg;
-%         x = (u_tg_v - u_tg_l)/(u_tg_v - u_tg_l);
-%         alpha = 1/( 1 + rho_tg_v/rho_tg_l * (1 - x)/x );
-%         rho_tg = alpha*rho_tg_v + (1 - alpha)*rho_tg_l;
+        [rho_tg_l, rho_tg_v, u_tg_l, u_tg_v] = n2o_fits_for_getting_P(P);
+        u_tg = U_tg/m_tg;
+        x = (u_tg_v - u_tg_l)/(u_tg_v - u_tg_l);
+        alpha = 1/( 1 + rho_tg_v/rho_tg_l * (1 - x)/x );
+        rho_tg = alpha*rho_tg_v + (1 - alpha)*rho_tg_l;
         
         rho_l = interp2(PDT.T, PDT.P, PDT.D_liq, T_l, P/1e3,'linear');
         
